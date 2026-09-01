3403de9006e2ad98911fed04695634f4858b94d8
Manas Mahodaya <manasmahodaya@hotmail.com>
2026-09-01 20:37:02 +0530

    Delete components/exercises.ts

--- components/exercises.ts
+++
@@ -1,8 +0,0 @@
-import exercises from "@/data/exercises.json";
-import type { Exercise } from "@/types/exercise";
-
-export const allExercises = exercises as Exercise[];
-
-export function getExercise(id: string) {
-  return allExercises.find((exercise) => exercise.id === id);
-}
